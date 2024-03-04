b6138865c2e0378bf59b97e744355698f6601197
ChillerDragon <ChillerDragon@gmail.com>
2024-03-04 14:49:16 +0800

    Run fix_style.py

--- src/rust-bridge-chillerbot/unicode.cpp
+++ src/rust-bridge-chillerbot/unicode.cpp
@@ -4,7 +4,7 @@ extern "C" {
 int cxxbridge1$str_width_unicode(const char *text) noexcept;
 } // extern "C"
 
-int str_width_unicode(const char *text) noexcept {
-  return cxxbridge1$str_width_unicode(text);
+int str_width_unicode(const char *text) noexcept
+{
+	return cxxbridge1$str_width_unicode(text);
 }
-
